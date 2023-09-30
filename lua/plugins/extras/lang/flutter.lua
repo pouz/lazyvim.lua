47b50b1c4d0376452d23bcc87c5ca2ce7793fc57
Pouz <whitewyrm84@gmail.com>
2023-09-30 11:10:46 +0900

    add flutter settings

--- lua/plugins/extras/lang/flutter.lua
+++ lua/plugins/extras/lang/flutter.lua
@@ -1,14 +1,75 @@
+--return {
+--  {
+--    "akinsho/flutter-tools.nvim",
+--    dependencies = { "nvim-lua/plenary.nvim" },
+--    config = function()
+--      require("flutter-tools").setup({
+--        lsp = {
+--          color = {
+--            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+--          },
+--        },
+--      })
+--    end,
+--  },
+--}
+if not require("config").pde.flutter then
+  return {}
+end
+
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "dart" })
+    end,
+  },
+  {
     "akinsho/flutter-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("flutter-tools").setup({
+    event = "VeryLazy",
+    opts = function()
+      local line = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
+      return {
+        ui = { border = line },
+        debugger = {
+          enabled = false,
+          run_via_dap = false,
+          exception_breakpoints = {},
+        },
+        outline = { auto_open = false },
+        decorations = {
+          statusline = { device = true, app_version = true },
+        },
+        widget_guides = { enabled = true, debug = false },
+        dev_log = { enabled = true, open_cmd = "tabedit" },
         lsp = {
           color = {
-            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            enabled = true,
+            background = true,
+            virtual_text = false,
+          },
+          settings = {
+            showTodos = false,
+            renameFilesWithClasses = "always",
+            updateImportsOnRename = true,
+            completeFunctionCalls = true,
+            lineLength = 100,
           },
         },
+      }
+    end,
+    dependencies = {
+      { "RobertBrunhage/flutter-riverpod-snippets" },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      { "sidlatau/neotest-dart" },
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.adapters, {
+        require("neotest-dart")({ command = "flutter" }),
       })
     end,
   },
