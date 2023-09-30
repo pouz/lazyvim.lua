ddad170bb0cbf45e6d7d75dfb9f118a715a46472
Pouz <whitewyrm84@gmail.com>
2023-09-30 11:23:19 +0900

    .

--- lua/plugins/extras/lang/flutter.lua
+++ lua/plugins/extras/lang/flutter.lua
@@ -1,72 +1,75 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "RobertBrunhage/flutter-riverpod-snippets",
+    },
+    config = function()
+      require("flutter-tools").setup({
+        lsp = {
+          color = {
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+          },
+        },
+      })
+    end,
+  },
+}
 --return {
 --  {
+--    "nvim-treesitter/nvim-treesitter",
+--    opts = function(_, opts)
+--      vim.list_extend(opts.ensure_installed, { "dart" })
+--    end,
+--  },
+--  {
 --    "akinsho/flutter-tools.nvim",
---    dependencies = { "nvim-lua/plenary.nvim" },
---    config = function()
---      require("flutter-tools").setup({
+--    event = "VeryLazy",
+--    opts = function()
+--      local line = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
+--      return {
+--        ui = { border = line },
+--        debugger = {
+--          enabled = false,
+--          run_via_dap = false,
+--          exception_breakpoints = {},
+--        },
+--        outline = { auto_open = false },
+--        decorations = {
+--          statusline = { device = true, app_version = true },
+--        },
+--        widget_guides = { enabled = true, debug = false },
+--        dev_log = { enabled = true, open_cmd = "tabedit" },
 --        lsp = {
 --          color = {
---            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+--            enabled = true,
+--            background = true,
+--            virtual_text = false,
+--          },
+--          settings = {
+--            showTodos = false,
+--            renameFilesWithClasses = "always",
+--            updateImportsOnRename = true,
+--            completeFunctionCalls = true,
+--            lineLength = 100,
 --          },
 --        },
+--      }
+--    end,
+--    dependencies = {
+--      { "RobertBrunhage/flutter-riverpod-snippets" },
+--    },
+--  },
+--  {
+--    "nvim-neotest/neotest",
+--    dependencies = {
+--      { "sidlatau/neotest-dart" },
+--    },
+--    opts = function(_, opts)
+--      vim.list_extend(opts.adapters, {
+--        require("neotest-dart")({ command = "flutter" }),
 --      })
 --    end,
 --  },
 --}
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "dart" })
-    end,
-  },
-  {
-    "akinsho/flutter-tools.nvim",
-    event = "VeryLazy",
-    opts = function()
-      local line = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
-      return {
-        ui = { border = line },
-        debugger = {
-          enabled = false,
-          run_via_dap = false,
-          exception_breakpoints = {},
-        },
-        outline = { auto_open = false },
-        decorations = {
-          statusline = { device = true, app_version = true },
-        },
-        widget_guides = { enabled = true, debug = false },
-        dev_log = { enabled = true, open_cmd = "tabedit" },
-        lsp = {
-          color = {
-            enabled = true,
-            background = true,
-            virtual_text = false,
-          },
-          settings = {
-            showTodos = false,
-            renameFilesWithClasses = "always",
-            updateImportsOnRename = true,
-            completeFunctionCalls = true,
-            lineLength = 100,
-          },
-        },
-      }
-    end,
-    dependencies = {
-      { "RobertBrunhage/flutter-riverpod-snippets" },
-    },
-  },
-  --  {
-  --    "nvim-neotest/neotest",
-  --    dependencies = {
-  --      { "sidlatau/neotest-dart" },
-  --    },
-  --    opts = function(_, opts)
-  --      vim.list_extend(opts.adapters, {
-  --        require("neotest-dart")({ command = "flutter" }),
-  --      })
-  --    end,
-  --  },
-}
